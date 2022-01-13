886c2961a8531ebb1cce36a9cc0fe1ba7b05b25d
Melody Wu <110306066@g.nccu.edu.tw>
2022-01-13 20:10:50 +0800

    add

---
+++ FinalProject/src/Credit.java
@@ -0,0 +1,21 @@
+
+public class Credit extends Purchase{
+	private double rate;
+	private int installment;
+	
+	public Credit(String name, int price) {
+		super(name, price);
+		this.rate = 0.06;
+		this.installment = 6;
+	}
+	
+	public double getPayment(int amount) {
+		double rateOfMonth = rate / 12;
+		double payment;
+		int period = installment * 12;
+		
+		payment = amount * (Math.pow((1+rateOfMonth),  period) * rateOfMonth)/(Math.pow(1+rateOfMonth, period)-1);
+		return payment;
+	}
+
+}
